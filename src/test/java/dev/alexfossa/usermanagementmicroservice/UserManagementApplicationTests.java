36fb5e1cdd24269187eefe3b0c9ea93588c0bb61
a.bakulin <a.bakulin@andersenlab.com>
2022-08-21 16:36:22 +0300

    [initial add]: added base project configuration

---
+++ src/test/java/dev/alexfossa/usermanagementmicroservice/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alexfossa.usermanagementmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
